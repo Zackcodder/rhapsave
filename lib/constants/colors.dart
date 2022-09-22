14c403e0748409c06e1f840e9c34577e96e608d0
zackcodder <zachtech56@gmail.com>
2022-09-22 14:26:38 +0100

    modified:   assets/fonts/Poppins-Bold.ttf 	modified:   assets/fonts/Poppins-Medium.ttf 	modified:   assets/fonts/Poppins-SemiBold.ttf 	renamed:    assets/fonts/images/changewallet.png -> assets/images/changewallet.png 	renamed:    assets/fonts/images/header.png -> assets/images/header.png 	renamed:    assets/fonts/images/home.png -> assets/images/home.png 	renamed:    assets/fonts/images/safe.png -> assets/images/safe.png 	renamed:    assets/fonts/images/verify.png -> assets/images/verify.png 	renamed:    assets/fonts/images/wallet.png -> assets/images/wallet.png 	new file:   lib/constants/colors.dart 	modified:   lib/home.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

---
+++ lib/constants/colors.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: constant_identifier_names, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+const kBackground = Color(0xffF5F5F5);
+const kNewHeaderColor = Color(0xff120A00);
+const kGeneralbodytextColor = Color(0xff484848);
+const kPorfileColor = Color(0xffF64F00);
+const kCircleColor = Color(0xffFFFFFF);
+
+
+const kTextfieldborderColor = Color(0xffE0E0E0);
+
+
+
+const kLoginbuttonColor = Color(0xffF9F9FA);
+const kSignupbuttonColor = Color(0xff119AE2);
+const kButtontextColor = Color(0xffF5F5F5);
+// const kSecndaryColor = Color(0xff52575C);
+
--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
-
+import 'package:rhapsave/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,17 +9,61 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: kBackground,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         toolbarHeight: 80,
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: Icon(Icons.menu_book_sharp, color: Colors.black,),
-      ),body: Column(
-        children: [
+        actions: [
           Container(
-            child: Text('Hello'),
+            margin: EdgeInsets.only(right: 24),
+            child: CircleAvatar(
+              child: Icon(
+                Icons.notifications_outlined,
+                size: 20,
+                color: kPorfileColor,
+              ),
+              backgroundColor: kCircleColor,
+              radius: 15,
+            ),
+          )
+        ],
+        leading: Container(
+          margin: EdgeInsets.only(left: 24),
+          child: CircleAvatar(
+            backgroundColor: kCircleColor,
+            child: Icon(
+              Icons.menu,
+              size: 20,
+              color: kGeneralbodytextColor,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: SizedBox(
+              child: Image.asset(
+                "assets/images/header.png",
+                fit: BoxFit.cover,
+                alignment: AlignmentDirectional.topStart,
+              ),
+            ),
+          ),
+          Stack(
+            alignment: AlignmentDirectional.centerStart,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 150),
+                child: Text('hello'),
+                color: kPorfileColor,
+              ),
+              Container(),
+            ],
           )
         ],
       ),
